9d033d3422d6744e194c729ba75007aa116890e1
Dmitry Shumilin <infernozzz@yandex.ru>
2020-09-04 22:47:14 +0300

    default ship names & s.h. ships added

--- YaacpPlanets/Planet.cs
+++ YaacpPlanets/Planet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using YaacpShips;
 using YaacpShips.Vessels;
 using YaacpShips.Cannons;
 
@@ -8,21 +9,76 @@ namespace YaacpPlanets
     {   
         public string[] ApplicantsTypes {get; private set;}
         public int[] ApplicantsAmount {get; protected set;}
+        public int MaturityLevel {get; protected set;}
+        public Vessel[] SecondhandShips {get; protected set;}
 
-        public Planet()
+        public Planet(int maturityLevel)
         {
             Random randomizer = new Random();
+
+            if (maturityLevel < 1) maturityLevel = 1;
+            else if (maturityLevel > 3) maturityLevel = 3;
+
+            this.MaturityLevel = maturityLevel;
             this.ApplicantsTypes = new string[4] {"troops", "sailors", "medics", "mechanics"};
             this.ApplicantsAmount = new int[this.ApplicantsTypes.Length];
             int max;
 
             for (var i = 0; i < this.ApplicantsTypes.Length; i++)
             {
-                if (this.ApplicantsTypes[i] == "troops") max = 500;
-                else if (this.ApplicantsTypes[i] == "sailors") max = 250;
-                else max = 100;
+                if (this.ApplicantsTypes[i] == "troops") max = 50;
+                else if (this.ApplicantsTypes[i] == "sailors") max = 25;
+                else max = 10;
 
+                max = max * 2 * this.MaturityLevel;
                 this.ApplicantsAmount[i] = randomizer.Next(0, max);
+            }
+
+            this.SecondhandShips = new Vessel[3 * this.MaturityLevel];
+            string shipName;
+
+            for (var i = 0; i < this.SecondhandShips.Length; i++)
+            {
+                shipName = BasicParameters.DefaultShipNames[randomizer.Next(0, BasicParameters.DefaultShipNames.Length - 1)];
+
+                switch (randomizer.Next(1, 3))
+                {
+                    case 1:
+                    this.SecondhandShips[i] = new Corvette(shipName);
+                    this.SecondhandShips[i].Armament = new Cannon[3];
+                    break;
+
+                    case 2:
+                    this.SecondhandShips[i] = new Frigate(shipName);
+                    this.SecondhandShips[i].Armament = new Cannon[6];
+                    break;
+
+                    case 3:
+                    this.SecondhandShips[i] = new Dreadnought(shipName);
+                    this.SecondhandShips[i].Armament = new Cannon[9];
+                    break;
+                }
+
+                for (var c = 0; c < this.SecondhandShips[i].Armament.Length; c++)
+                {
+                    switch (randomizer.Next(1, 3))
+                    {
+                        case 1:
+                        this.SecondhandShips[i].Armament[c] = new Laser(randomizer.Next(1, this.SecondhandShips[i].Size)) {Load = true};
+                        break;
+
+                        case 2:
+                        this.SecondhandShips[i].Armament[c] = new Kinetic(randomizer.Next(1, this.SecondhandShips[i].Size)) {Load = true};
+                        break;
+
+                        case 3:
+                        this.SecondhandShips[i].Armament[c] = new Rocket(randomizer.Next(1, this.SecondhandShips[i].Size)) {Load = true};
+                        break;
+                    }
+                }
+
+                this.SecondhandShips[i].GetDamage(randomizer.Next(1, this.SecondhandShips[i].HealthMax - 1));
+                this.SecondhandShips[i].Status = "For sale";
             }
         }
 
@@ -53,5 +109,7 @@ namespace YaacpPlanets
         {
             ship.Repair(value);
         }
+
+
     }
 }
--- YaacpShips/BasicParameters.cs
+++ YaacpShips/BasicParameters.cs
@@ -4,10 +4,36 @@ namespace YaacpShips
 {
     public static class BasicParameters
     {
+        private static int troopsPowerMod = 3;
+        private static int sailorsPowerMod = 2;
+        private static int othersPowerMod = 1;
+
         public static int HealthBaseDefault => 500;
-        private static int TroopsPowerMod = 3;
-        private static int SailorsPowerMod = 2;
-        private static int OthersPowerMod = 1;
+        public static string[] DefaultShipNames => new string[] {
+            "Lola",
+            "Rosetta",
+            "Celestia",
+            "Hecate",
+            "Eridae",
+            "Queen Anna's Revenge",
+            "Enterprise",
+            "Sentinel",
+            "Bearded Bastard",
+            "Black Knight",
+            "Cyber Bieber",
+            "Luna",
+            "Sepia",
+            "Seline",
+            "Mayflower",
+            "Mandalorian",
+            "Apostle",
+            "Galadriel",
+            "Gandalf",
+            "Boromir",
+            "Vladimir",
+            "Admiral Kuznetsoff",
+            "Magus"
+        };
 
         public static int BoardingPower(Vessel ship)
         {
@@ -16,9 +42,9 @@ namespace YaacpShips
 
             for (var i = 0; i < ship.Crew.Length; i++)
             {
-                if (ship.CrewTypes[i] == "troops") mod = BasicParameters.TroopsPowerMod;
-                else if (ship.CrewTypes[i] == "sailors") mod = BasicParameters.SailorsPowerMod;
-                else mod = BasicParameters.OthersPowerMod;
+                if (ship.CrewTypes[i] == "troops") mod = BasicParameters.troopsPowerMod;
+                else if (ship.CrewTypes[i] == "sailors") mod = BasicParameters.sailorsPowerMod;
+                else mod = BasicParameters.othersPowerMod;
 
                 result += ship.Crew[i] * mod;
             }
@@ -34,9 +60,9 @@ namespace YaacpShips
 
             for (var i = 0; i < crewTypes.Length; i++)
             {
-                if (crewTypes[i] == "troops") mod = BasicParameters.TroopsPowerMod;
-                else if (crewTypes[i] == "sailors") mod = BasicParameters.SailorsPowerMod;
-                else mod = BasicParameters.OthersPowerMod;
+                if (crewTypes[i] == "troops") mod = BasicParameters.troopsPowerMod;
+                else if (crewTypes[i] == "sailors") mod = BasicParameters.sailorsPowerMod;
+                else mod = BasicParameters.othersPowerMod;
 
                 result[i] = crew[i] - (((crew[i] * 100) / powerPercentage) / mod);
             }
@@ -51,9 +77,9 @@ namespace YaacpShips
 
             for (var i = 0; i < crewTypes.Length; i++)
             {
-                if (crewTypes[i] == "troops") mod = BasicParameters.TroopsPowerMod;
-                else if (crewTypes[i] == "sailors") mod = BasicParameters.SailorsPowerMod;
-                else mod = BasicParameters.OthersPowerMod;
+                if (crewTypes[i] == "troops") mod = BasicParameters.troopsPowerMod;
+                else if (crewTypes[i] == "sailors") mod = BasicParameters.sailorsPowerMod;
+                else mod = BasicParameters.othersPowerMod;
 
                 result[i] = crew[i] - (((crew[i] * 100) / powerPercentage) / mod);
             }
