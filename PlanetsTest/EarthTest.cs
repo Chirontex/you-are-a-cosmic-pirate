3dce21665ee6b7befd85358e3021d17b3faf0c13
Dmitry Shumilin <infernozzz@yandex.ru>
2020-09-09 02:01:09 +0300

    Earth unit test updated

--- PlanetsTest/EarthTest.cs
+++ PlanetsTest/EarthTest.cs
@@ -22,6 +22,13 @@ namespace PlanetsTest
             {
                 this.ship.Armament[i] = new Kinetic(2) {Load = true};
             }
+
+            for (var i = 0; i < this.ship.CrewTypes.Length; i++)
+            {
+                if (this.ship.CrewTypes[i] == "troops") this.ship.Crew[i] = 50;
+                else if (this.ship.CrewTypes[i] == "sailors") this.ship.Crew[i] = 40;
+                else this.ship.Crew[i] = 6;
+            }
         }
 
         [TestMethod]
@@ -47,6 +54,31 @@ namespace PlanetsTest
             Assert.AreEqual(expected, this.ship.Status,
                 String.Format("Expected ship status: {0}; actual: {1}.",
                     expected, this.ship.Status));
+        }
+
+        [TestMethod]
+        public void TestFiring()
+        {
+            this.TestInit();
+
+            int medicsOnPlanetIndex = Array.IndexOf(this.planet.ApplicantsTypes, "medics");
+            
+            Assert.AreNotEqual(medicsOnPlanetIndex, (this.planet.ApplicantsTypes.GetLowerBound(0) - 1),
+                String.Format("Medics on planet index must not be equal to {0}, but it does.",
+                    (this.planet.ApplicantsTypes.GetLowerBound(0) - 1)));
+
+            int medicsOnPlanet = this.planet.ApplicantsAmount[medicsOnPlanetIndex];
+            int medicsOnShip = this.ship.Crew[Array.IndexOf(this.ship.CrewTypes, "medics")];
+
+            this.planet.Firing(this.ship, "medics", 5);
+
+            Assert.AreEqual((medicsOnPlanet + 5), this.planet.ApplicantsAmount[medicsOnPlanetIndex],
+                String.Format("Medics on planet amount expected: {0}; actual: {1}.",
+                    (medicsOnPlanet + 5), this.planet.ApplicantsAmount[medicsOnPlanetIndex]));
+
+            Assert.AreEqual(medicsOnShip, (this.ship.Crew[Array.IndexOf(this.ship.CrewTypes, "medics")] + 5),
+                String.Format("Medics on ship expected: {0}; actual: {1}.",
+                    medicsOnShip, (this.planet.ApplicantsAmount[Array.IndexOf(this.ship.CrewTypes, "medics")] + 5)));
         }
     }
 }
