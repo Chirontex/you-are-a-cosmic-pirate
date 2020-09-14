8a1033b6cc59e7dc1fb1340e97864792fc673cd7
Dmitry Shumilin <infernozzz@yandex.ru>
2020-09-14 09:53:45 +0300

    crew assignment refactored

--- Yaacp/Program.cs
+++ Yaacp/Program.cs
@@ -15,18 +15,17 @@ namespace Yaacp
             string shipName = Console.ReadLine();
             Vessel ship = new Corvette(shipName);
             ship.Armament = new Cannon[3];
-            int credits = 10000;
 
             for (var i = 0; i < ship.Armament.Length; i++)
             {
                 ship.Armament[i] = new Laser(1) {Load = true};
             }
 
+            ship.GetCrew("troops", 40);
+            ship.GetCrew("sailors", 30);
+
             int troopsIndex = Array.IndexOf(ship.CrewTypes, "troops");
             int sailorsIndex = Array.IndexOf(ship.CrewTypes, "sailors");
-
-            ship.Crew[troopsIndex] = 40;
-            ship.Crew[sailorsIndex] = 30;
 
             Console.WriteLine($"\nВаш корабль — корвет по имени {ship.Name}. В данный момент он имеет {ship.Armament.Length} лазерных пушки и {ship.Crew[0] + ship.Crew[1]} человек экипажа — {ship.Crew[sailorsIndex]} матросов и {ship.Crew[troopsIndex]} солдат.");
             Console.ReadKey();
